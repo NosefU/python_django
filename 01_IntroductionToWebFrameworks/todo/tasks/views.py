627131c1d12eab8fe1049267d28a1d3dca19a249
Valko Dmitry <nosefu@gmail.com>
2021-10-25 22:51:23 +0300

    feat(01_IntroductionToWebFrameworks.todo.tasks) исправлено в соответствии с уточнённым заданием

--- 01_IntroductionToWebFrameworks/todo/tasks/views.py
+++ 01_IntroductionToWebFrameworks/todo/tasks/views.py
@@ -1,15 +1,34 @@
+import random
+from typing import List
+
 from django.http import HttpResponse
 
 from django.views import View
 
 
+def get_tasks(count: int = 5) -> List[str]:
+    tasks = [
+        'PSP: buy new housing shell',
+        'Wii: replace broken USB ports',
+        'Famicom: retrobright housing shell',
+        'Dreamcast: buy new gamepad',
+        'PS2: Buy network adapter',
+        'PS1: Buy PSIO',
+        'X360: buy wide angle lens for kinect',
+        'PSP: buy microsd to memory stick duo adapter',
+        'Famicom: buy CoolGirl',
+        'PVM: replace resistor on sync channer',
+    ]
+    if len(tasks) < count:
+        count = len(tasks)
+    return random.sample(tasks, count)
+
+
 class ToDoView(View):
 
     def get(self, request, *args, **kwargs):
-        return HttpResponse('<ul>'
-                            '<li>Установить python</li>'
-                            '<li>Установить django</li>'
-                            '<li>Запустить сервер</li>'
-                            '<li><b>Изменить список дел</b></li>'
-                            '<li><b>Закоммитить и отправить на проверку</b></li>'
-                            '</ul>')
+        content = '<ul>'
+        for task in get_tasks(5):
+            content += f'<li>{task}</li>'
+        content += '</ul>'
+        return HttpResponse(content)
