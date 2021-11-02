fb85571f428ff1cc4111003188b7dfc876a6dd7a
Valko Dmitry <nosefu@gmail.com>
2021-11-02 22:57:12 +0300

    feat(03_RequestsHandlingInDjango) добавлены вьюхи, шаблоны и роутинги для контактов и описания компании

--- 03_RequestsHandlingInDjango/board/advertisements/urls.py
+++ 03_RequestsHandlingInDjango/board/advertisements/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.advertisement_list, name='advertisement_list'),
     path('advertisements', views.Advertisements.as_view(), name='advertisements'),
+    path('contacts', views.Contacts.as_view(), name='contacts'),
+    path('about', views.About.as_view(), name='about'),
 ]
--- 03_RequestsHandlingInDjango/board/advertisements/views.py
+++ 03_RequestsHandlingInDjango/board/advertisements/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import TemplateView
 
 ADV_LIST = [
     'Nintendo Switch',
@@ -26,6 +27,35 @@ class Advertisements(View):
         return HttpResponse(f'Объявление успешно добавлено на площадку<br>'
                             f'Добавлено объявлений: {post_requests}')
 
+
+class Contacts(TemplateView):
+    template_name = 'advertisements/contacts.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'address': 'City, Street 1, office 1',
+            'phone': '+7 999 999-99-99',
+            'email': 'mail@mail.com'
+        })
+        return context
+
+
+class About(TemplateView):
+    template_name = 'advertisements/about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'company_name': 'ООО «Вектор»',
+            'description': """Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor 
+            in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur 
+            sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est 
+            laborum."""
+        })
+        return context
 
 def advertisement_list(request, *args, **kwargs):
     advertisements = [
