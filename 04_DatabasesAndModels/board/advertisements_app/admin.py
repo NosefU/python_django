bcb70b3792d6d150a8dc21366b2d529001060fb7
Valko Dmitry <nosefu@gmail.com>
2021-11-06 22:31:32 +0300

    feat(04_DatabasesAndModels) модели добавлены в админку. заполнена БД

--- 04_DatabasesAndModels/board/advertisements_app/admin.py
+++ 04_DatabasesAndModels/board/advertisements_app/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
+from advertisements_app.models import Advertisement, User, AdvertisementCategory, AdvertisementStatus
 # Register your models here.
+
+
+@admin.register(Advertisement)
+class AdvertisementAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(AdvertisementCategory)
+class AdvertisementCategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(AdvertisementStatus)
+class AdvertisementStatusAdmin(admin.ModelAdmin):
+    pass
