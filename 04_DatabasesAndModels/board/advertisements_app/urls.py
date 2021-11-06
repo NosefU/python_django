64141951e9fc3a730029cab6302b6a843b52ef3d
Valko Dmitry <nosefu@gmail.com>
2021-11-06 23:23:01 +0300

    feat(04_DatabasesAndModels) добавлены вьюхи и шаблоны для списка объявлений и страницы отдельного объявления

--- 04_DatabasesAndModels/board/advertisements_app/urls.py
+++ 04_DatabasesAndModels/board/advertisements_app/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
-urlpatterns = [
+from advertisements_app.views import AdvertisementsListView, AdvertisementsDetailView
 
+urlpatterns = [
+    path('advertisements', AdvertisementsListView.as_view(), name='advertisements_list'),
+    path('advertisements/<int:pk>', AdvertisementsDetailView.as_view(), name='advertisements_detail'),
 ]
--- 04_DatabasesAndModels/board/advertisements_app/views.py
+++ 04_DatabasesAndModels/board/advertisements_app/views.py
@@ -1,1 +1,23 @@
 from django.shortcuts import render
+from django.views import generic
+
+RUB_USD_EXCHANGE_RATE = 71.15
+
+from advertisements_app.models import Advertisement
+
+
+class AdvertisementsListView(generic.ListView):
+    model = Advertisement
+    context_object_name = 'advertisements'
+    queryset = Advertisement.objects.all()[:10]
+
+
+class AdvertisementsDetailView(generic.DetailView):
+    model = Advertisement
+    context_object_name = 'advertisement'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['usd_price'] = context['advertisement'].price / RUB_USD_EXCHANGE_RATE
+        # TODO простейшая реализация, в дальнейшем можно будет кэшировать откуда-нибудь курс и тянуть его из кэша
+        return context
