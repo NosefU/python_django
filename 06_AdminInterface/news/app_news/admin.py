ec5eefdfb3317dc2324085a8484b243e040ff188
Valko Dmitry <nosefu@gmail.com>
2021-11-21 20:39:33 +0300

    feat(06_AdminInterface) Реализованы CommentInline и привязан к ArticleAdmin

--- 06_AdminInterface/news/app_news/admin.py
+++ 06_AdminInterface/news/app_news/admin.py
@@ -2,13 +2,21 @@ from django.contrib import admin
 from app_news.models import Article, Comment
 
 
-@admin.register(Article)
-class ArticleAdmin(admin.ModelAdmin):
-    list_display = ['title', 'subtitle', 'image', 'created', 'modified', 'active']
-    list_filter = ['active']
-
-
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = [field.name for field in Comment._meta.get_fields()]
     list_filter = ['username']
+
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['title', 'subtitle', 'image', 'created', 'modified', 'active']
+    list_filter = ['active']
+    inlines = [CommentInline, ]
+
+
+
