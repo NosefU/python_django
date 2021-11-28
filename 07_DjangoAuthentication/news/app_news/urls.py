c5d4150bfe788dd3d6d1d74730ff927ff630c365
Valko Dmitry <nosefu@gmail.com>
2021-11-28 14:07:13 +0300

    feat(07_DjangoAuthentication) Добавлен функционал логина

---
+++ 07_DjangoAuthentication/news/app_news/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls.static import static
+from django.urls import path
+from app_news.views import ArticleList, ArticleDetail, AddArticle, EditArticle, NewsLoginView
+from news import settings
+
+urlpatterns = [
+    path('', ArticleList.as_view(), name='article_list'),
+    path('article/<int:article_id>', ArticleDetail.as_view(), name='article_detail'),
+    path('article/add', AddArticle.as_view(), name='add_article'),
+    path('article/<int:article_id>/edit', EditArticle.as_view(), name='edit_article'),
+    path('login', NewsLoginView.as_view(), name='login'),
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
