018efc345384e7901aceacc566ac19658443c8ed
Valko Dmitry <nosefu@gmail.com>
2023-01-25 00:31:36 +0300

    feat(11_i18n) Модели приложения app_blog подготовлены к локализации

--- 11_I18n/djfiles/app_blog/models.py
+++ 11_I18n/djfiles/app_blog/models.py
@@ -1,17 +1,20 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class BlogRecord(models.Model):
-    title = models.CharField(max_length=200, blank=True, verbose_name='Заголовок')
-    body = models.TextField(verbose_name='Текст записи')
-    created = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    title = models.CharField(max_length=200, blank=True, verbose_name=_('Заголовок'))
+    body = models.TextField(verbose_name=_('Текст записи'))
+    created = models.DateTimeField(auto_now_add=True, verbose_name=_('Время создания'))
     author = models.ForeignKey(User, on_delete=models.CASCADE,
-                               related_name='records', verbose_name='Автор')
-    cover = models.ImageField(upload_to='blog_images', blank=True, verbose_name='Обложка')
+                               related_name='records', verbose_name=_('Автор'))
+    cover = models.ImageField(upload_to='blog_images', blank=True, verbose_name=_('Обложка'))
 
     class Meta:
         ordering = ['-created', ]
+        verbose_name_plural = _('blog_records')
+        verbose_name = _('blog_record')
 
     def __repr__(self):
         return f"BlogRecord {self.id}: user {self.author.id}, {self.title}"
